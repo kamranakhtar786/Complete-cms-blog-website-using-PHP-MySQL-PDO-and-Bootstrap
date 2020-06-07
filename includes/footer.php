13804af02d94168dd31a70f11c2f43620f431baa
K A FAROOQUI <Kamranakhtar2018@gmail.com>
2020-06-07 17:30:58 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,8 @@
 
 <div class="col-lg-15 bg-dark text-light py-3 justify-content-center">
 <footer class="text-center">
-        <span>Copyright &copy; All Right Reserved</span>
+        <span>Copyright &copy; All Right Reserved</span><br>
+        <span>Created by <a href="https://kafarooqui.in">Kafarooqui.in</a></span>
       </footer>
 
 </div>
